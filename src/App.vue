f6bd8e7f0f619cfb5351e03cdbd7118505a0d017
tangliang <tangliang@uspring.cn>
2025-03-04 18:48:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!',
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/views/demo/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- 页面结构 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data() {
+    return {
+      // ... existing data ...
+      demoData: 'demo数据',
+    }
+  },
+  created() {
+    console.log('组件实例创建完成')
+  },
+  mounted() {
+    console.log('DOM挂载完成')
+  },
+  methods: {
+    demoFn() {
+      console.log('demo方法')
+    },
+  },
+}
+</script>
+<style lang="less" scoped></style>
